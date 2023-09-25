492b7465d71bc02ac5cda65d2c6990281a502b5c
Jacuancum <cks2306@gmail.com>
2023-09-25 10:03:15 +0800

    w02 cse121b add image

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,21 +3,28 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+let fullName = 'Jacob Chan';
+let currentYear = new Date();
+let profilePicture = 'images/my_image.jpeg';
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById('name');
+let foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.getElementById('my_image.jpeg');
 
 /* Step 4 - Adding Content */
+nameElement.innerHTML = <strong>${fullName}</strong>;
+yearElement.textContent = ${currentYear};
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', Profile image of${fullName});
 
 
-
-
+/* Step 5 - Array */
+let favoriteFood = ['Apple Pie', 'Sashimi', 'Dark Chocolate', 'Hot Spring Egg'];
+let moreFavFood = favoriteFood.join(', ');
+let item = foodElement.innerHTML = <strong>${moreFavFood}</strong>;
+console.log(item);
 
 
 /* Step 5 - Array */

1e63866e49955d47f2affddfe9c44c6e612d8fe6
Jacuancum <cks2306@gmail.com>
2023-09-25 10:25:27 +0800

    w02 cse121b add

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -5,29 +5,30 @@
 /* Step 2 - Variables */
 let fullName = 'Jacob Chan';
 let currentYear = new Date();
-let profilePicture = 'images/my_image.jpeg';
+let profilePicture = 'images/myimage.jpeg'
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById('name');
+let nameElement = document.getElementById('name');
 let foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.getElementById('my_image.jpeg');
+const imageElement = document.getElementById('myimage.jpeg');
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = <strong>${fullName}</strong>;
-yearElement.textContent = ${currentYear};
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `${currentYear}`;
 imageElement.setAttribute('src', profilePicture);
-imageElement.setAttribute('alt', Profile image of${fullName});
+imageElement.setAttribute('alt', profilePicture);
 
 
 /* Step 5 - Array */
 let favoriteFood = ['Apple Pie', 'Sashimi', 'Dark Chocolate', 'Hot Spring Egg'];
+
+
 let moreFavFood = favoriteFood.join(', ');
 let item = foodElement.innerHTML = <strong>${moreFavFood}</strong>;
-console.log(item);
+foodElement.innerHTML += `${favoriteFood}`;
+foodElement.innerHTML += `<br>`;
 
-
-/* Step 5 - Array */
 
 
 

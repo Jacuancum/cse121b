3948f1b412d18010f957fe5084efdc25da7b647d
Jacuancum <cks2306@gmail.com>
2023-10-25 09:54:38 +0800

    cse121b_final_project

--- final_project.js
+++ final_project.js
@@ -1,26 +1,35 @@
 import {makeGuess} from "./makeGuess.js";
 
-const words = ["computer", "program", "game", "isabella", "jacob", "joseph", "benjamin", "mormon"];
+const words = ["computer", "program", "game", "javascript", "jacob", "joseph", "benjamin", "mormon"];
 
-let wordToGuess;
+let hiddenWord;
 let guessedWord;
 let attempts;
 
-function initializeGame() {
-    wordToGuess = chooseRandomWord(words);
+function theGame() {
+    hiddenWord = chooseRandomWord(words);
     
-    guessedWord = Array(wordToGuess.length).fill("_");
+    guessedWord = Array(hiddenWord.length).fill("_");
     attempts = 0;
-    
     updateDisplay();
 }
 
 function chooseRandomWord(wordArray) {
-    
+    const randomIndex = Math.floor(Math.random() * wordArray.length);
+    const randomWord = wordArray[randomIndex];
+    return randomWord;
 }
 
 function updateDisplay() {
-    
+    document.getElementById("word_display").textContent = guessedWord.join(" ");
+    document.getElementById("message").textContent = "";
+    document.getElementById("attempts").textContent = attempts;
 }
 
-document.addEventListener();
+document.addEventListener("DOMContentLoaded", () => {
+    theGame();
+    document.getElementById("guess_button").addEventListener("click", () => {
+        const letter = document.getElementById("letter_input").value;
+        makeGuess(letter);
+    });
+});
--- makeGuess.js
+++ makeGuess.js
@@ -1,9 +1,22 @@
 export function makeGuess(letter) {
-    
+    if (guessedWord.join("") === hiddenWord) {
+        return;
+    }
 
-    const letterIndex = wordToGuess.indexOf(letter);
-    
+    const letterIndex = hiddenWord.indexOf(letter);
+    if (letterIndex === -1) {
+        attempts++;
+        updateDisplay();
+    } else {
+        while (letterIndex !== -1) {
+            guessedWord[letterIndex] = letter;
+            letterIndex = hiddenWord.indexOf(letter, letterIndex + 1);
+        }
+    }
 
     updateDisplay();
-    
+    document.getElementById("letter_input").value = "";
+    if (guessedWord.join("") === hiddenWord) {
+        document.getElementById("message").textContent = "Congratulations! You guessed the word.";
+    }
 }
